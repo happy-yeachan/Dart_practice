d6c9681afae5e70746031020bd1130882a01abb3
yeachan <dus001228@naver.com>
2024-03-21 15:58:51 +0900

    Defining a Function

--- functions.dart
+++ functions.dart
@@ -1,1 +1,8 @@
-void main() {}
+// String sayHello(String name) {
+//   return "Hello $name nice to meet you!";
+// }
+// 위의 코드를 아래처럼 단축하여 사용 가능
+String sayHello(String name) => "Hello $name nice to meet you!";
+void main() {
+  print(sayHello('yeachan'));
+}
