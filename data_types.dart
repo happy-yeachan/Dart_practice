0bb5f6bebe4fa6c4de55fa54ae35fa90c13060f7
yeachan <dus001228@naver.com>
2024-03-21 15:19:17 +0900

    list 내부에 조건 넣기

--- data_types.dart
+++ data_types.dart
@@ -4,7 +4,14 @@ void main() {
   a = 1.1; //이런 식으로 가능함
 
   // list 선언 방식
+  var givemefive = true;
   var numbers = [1, 2, 3, 4];
-  List<int> numbers2 = [1, 2, 3, 4];
+  List<int> numbers2 = [
+    1,
+    2,
+    3,
+    4,
+    if (givemefive) 5, //안에 조건에 따라 들어갈 요소도 지정 가능!
+  ];
   numbers.add(2);
 }
