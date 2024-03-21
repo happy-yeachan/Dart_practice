02f0e5668ffd48b0e24f2d817ed91f52a15085b5
yeachan <dus001228@naver.com>
2024-03-21 14:41:09 +0900

    dynamic 선언

--- main.dart
+++ main.dart
@@ -5,5 +5,13 @@ void main() {
   var i = "hello";
   String j = "bye";
   i = "yeachan";
+
+  // 다이나믹 형식으로 선언하면 모든 타입을 가질 수 있음
+  // var만 사용하여 선언하면 다이나믹 형식을 가짐
+  // dynamic으로도 선언 가능
+  var d;
+  dynamic dd;
+  d = 2;
+  d = "yes";
   print(i);
 }
