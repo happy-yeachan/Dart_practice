3993b50e5428060bf29a665f77725446b057c645
yeachan <dus001228@naver.com>
2024-03-21 15:17:29 +0900

    list 선언

--- data_types.dart
+++ data_types.dart
@@ -2,4 +2,9 @@ void main() {
   // num은 int와 double의 부모 클래스!! 미친
   num a = 1;
   a = 1.1; //이런 식으로 가능함
+
+  // list 선언 방식
+  var numbers = [1, 2, 3, 4];
+  List<int> numbers2 = [1, 2, 3, 4];
+  numbers.add(2);
 }
